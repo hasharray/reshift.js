043fb06a4524fdd166f944ed2447dcb445297f90
Casper Beyer <caspervonb@users.noreply.github.com>
2016-10-17 01:15:56 +0800

    Remove development dependency on tape

--- test/variable.js
+++ test/variable.js
@@ -1,59 +1,55 @@
-var test = require('tape');
+var assert = require('assert');
 var rewrite = require('..');
 
-test('rewrite variable', function(assert) {
-  var input = [
-    'var answer = 6 * 7',
-  ].join('\n')
+var input = [
+  'var answer = 6 * 7',
+].join('\n')
 
-  var output = rewrite(input, function (node) {
-    if (node.type == 'Program') {
-      assert.equal(node.toString(), 'let answer = 1 * 2');
-      assert.equal(node.parent, undefined);
-    }
+var output = rewrite(input, function (node) {
+  if (node.type == 'Program') {
+    assert.equal(node.toString(), 'let answer = 1 * 2');
+    assert.equal(node.parent, undefined);
+  }
 
-    if (node.type == 'VariableDeclaration') {
-      assert.equal(node.kind, 'var');
-      assert.equal(node.declarations[0].type, 'VariableDeclarator');
-      assert.equal(node.declarations[0].id.type, 'Identifier');
-      assert.equal(node.declarations[0].id.name, 'answer');
+  if (node.type == 'VariableDeclaration') {
+    assert.equal(node.kind, 'var');
+    assert.equal(node.declarations[0].type, 'VariableDeclarator');
+    assert.equal(node.declarations[0].id.type, 'Identifier');
+    assert.equal(node.declarations[0].id.name, 'answer');
 
-      assert.equal(node.parent.type, 'Program');
+    assert.equal(node.parent.type, 'Program');
 
-      return node.toString().replace('var', 'let');
-    }
+    return node.toString().replace('var', 'let');
+  }
 
-    if (node.type == 'VariableDeclarator') {
-      assert.equal(node.id.type, 'Identifier');
-      assert.equal(node.id.name, 'answer');
+  if (node.type == 'VariableDeclarator') {
+    assert.equal(node.id.type, 'Identifier');
+    assert.equal(node.id.name, 'answer');
 
-      assert.equal(node.parent.type, 'VariableDeclaration');
-    }
+    assert.equal(node.parent.type, 'VariableDeclaration');
+  }
 
-    if (node.type == 'BinaryExpression') {
-      assert.equal(node.operator, '*');
+  if (node.type == 'BinaryExpression') {
+    assert.equal(node.operator, '*');
 
-      assert.equal(node.left.value, 6);
-      assert.equal(node.left.raw, '6');
-      assert.equal(node.left.toString(), '1');
+    assert.equal(node.left.value, 6);
+    assert.equal(node.left.raw, '6');
+    assert.equal(node.left.toString(), '1');
 
-      assert.equal(node.right.value, 7);
-      assert.equal(node.right.raw, '7');
-      assert.equal(node.right.toString(), '2');
+    assert.equal(node.right.value, 7);
+    assert.equal(node.right.raw, '7');
+    assert.equal(node.right.toString(), '2');
 
-      assert.equal(node.parent.type, 'VariableDeclarator');
-    }
+    assert.equal(node.parent.type, 'VariableDeclarator');
+  }
 
-    if (node.type == 'Literal') {
-      return (node.value == 6) ? 1 : 2;
-    }
+  if (node.type == 'Literal') {
+    return (node.value == 6) ? 1 : 2;
+  }
 
-    return node.toString();
-  });
-
-  assert.equal(output.code, [
-    'let answer = 1 * 2',
-  ].join('\n'));
-
-  assert.end();
+  return node.toString();
 });
+
+assert.equal(output.code, [
+  'let answer = 1 * 2',
+].join('\n'));
